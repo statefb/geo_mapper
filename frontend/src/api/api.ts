0c50d9411a1e5466f57d63dca6953e91b46c2974
statefb <tmgf.15.su@gmail.com>
2022-03-30 15:12:10 +0900

    fix: api key

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -6,11 +6,11 @@ let options = undefined;
 if (process.env.REACT_APP_USE_MOCK === "true") {
   url = "";
 } else {
-  url =
-    "https://7we7bhac3nc6dp2vxhcrv6wmqi.appsync-api.ap-northeast-1.amazonaws.com/graphql";
+  url = `https://${process.env.REACT_APP_APPSYNC_API_ID}.appsync-api.${process.env.REACT_APP_AWS_REGION}.amazonaws.com/graphql`;
+  // "https://7we7bhac3nc6dp2vxhcrv6wmqi.appsync-api.ap-northeast-1.amazonaws.com/graphql";
   options = {
     headers: {
-      "x-api-key": "da2-wez37mni7bb7bez3xppono4ecm",
+      "x-api-key": process.env.REACT_APP_APPSYNC_API_KEY || "",
     },
   };
 }
